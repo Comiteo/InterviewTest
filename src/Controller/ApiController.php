869478997b6b56ae587019065fcb5a3b17aa797a
Etienne TRAN <etiennetran@hotmail.fr>
2022-07-26 20:18:34 +0200

    test_etienne : refacto articles + create post

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,53 +4,98 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
+    private NormalizerInterface $normalizer;
+    private SerializerInterface $serializer;
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * @param NormalizerInterface $normalizer
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(NormalizerInterface $normalizer, SerializerInterface $serializer, EntityManagerInterface $entityManager)
+    {
+        $this->normalizer = $normalizer;
+        $this->serializer = $serializer;
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/api/v1/articles", name="api_v1_article_cget", methods={"GET"})
      */
-    public function cget()
+    public function getArticles(): JsonResponse
     {
-        $em = $this->getDoctrine()->getManager();
-        $articles = $em->getRepository('App:Article')->findAll();
-        $normalizedArticles = [];
-
-        foreach ($articles as $article) {
-            $normalizedArticle = [
-                "title" => $article->getTitle(),
-                "content" => $article->getContent(),
-                "author" => $article->getAuthor(),
-                "created_at" => $article->getCreatedAt()->format('c'),
-                "updated_at" => $article->getUpdatedAt()->format('c'),
-                "uri" => $this->generateUrl('api_v1_article_get', ["id" => $article->getId()]),
-            ];
-
-            $normalizedArticles[] = $normalizedArticle;
-        }
-
-        return $this->json($normalizedArticles);
+        $articles = $this->entityManager->getRepository('App:Article')->findAll();
+        return $this->json($this->normalizeArticles($articles));
     }
 
     /**
      * @Route("/api/v1/articles/{id}", name="api_v1_article_get", methods={"GET"})
      */
-    public function getOne(int $id)
+    public function getArticleById(int $id) : JsonResponse
     {
-        $em = $this->getDoctrine()->getManager();
-        $article = $em->getRepository('App:Article')->find($id);
+        $article = $this->entityManager->getRepository('App:Article')->find($id);
 
-        $normalizedArticle = [
-            "title" => $article->getTitle(),
-            "content" => $article->getContent(),
-            "author" => $article->getAuthor(),
-            "created_at" => $article->getCreatedAt()->format('c'),
-            "updated_at" => $article->getUpdatedAt()->format('c'),
-            "uri" => $this->generateUrl('api_v1_article_get', ["id" => $article->getId()]),
-        ];
+        if(!$article) {
+            return $this->json(['404' => 'The article does not exist.']);
+        }
 
-        return $this->json($normalizedArticle);
+        return $this->json($this->normalizeArticle($article));
+    }
+
+    /**
+     * @Route("/api/v1/articles", name="api_v1_article_post", methods={"POST"})
+     */
+    public function createArticle(Request $request): JsonResponse
+    {
+        $body = $request->getContent();
+
+        $article = $this->serializer->deserialize($body, Article::class, 'json');
+
+        $article->setCreatedAt(new DateTime())
+            ->setUpdatedAt(new DateTime());
+
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+
+        return $this->json($this->normalizeArticle($article));
+    }
+
+    /**
+     * @param Article $article
+     *
+     * @return array
+     */
+    private function normalizeArticle(Article $article): array
+    {
+        $normalizedArticle = $this->normalizer->normalize($article);
+        $normalizedArticle['uri'] = $this->generateUrl('api_v1_article_get', ["id" => $article->getId()]);
+
+        return $normalizedArticle;
+    }
+
+    /**
+     * @param array<Article> $articles
+     *
+     * @return array
+     */
+    private function normalizeArticles(array $articles): array
+    {
+        foreach ($articles as $article) {
+            $normalizedArticles[] = $this->normalizeArticle($article);
+        }
+
+        return $normalizedArticles ?? [];
     }
 }
