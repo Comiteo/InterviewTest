97850a9ed4945fba4e97d888646354287d0240d7
Guillaume D <gdeloince.consulting@gmail.com>
2022-12-16 00:51:19 +0100

    feat(author): add controller to get counter for a specific author

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Author\ArticlesCounterInterface;
+use App\Entity\Author;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    private ArticlesCounterInterface $articlesCounter;
+
+    public function __construct(ArticlesCounterInterface $articlesCounter) {
+        $this->articlesCounter = $articlesCounter;
+    }
+
+    /**
+     * @Route("/api/v1/authors/{id}/counter", name="api_v1_authors_counter", methods={"GET"})
+     * @ParamConverter("author", class="App:Author")
+     */
+    public function counter(Author $author): JsonResponse
+    {
+        return $this->json([
+            'counter' => $this->articlesCounter->countArticles($author)
+        ]);
+    }
+}
