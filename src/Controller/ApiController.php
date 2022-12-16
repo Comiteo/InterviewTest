4f4e9ba14fd3b72833d2489d6a2e1b41270a043b
Guillaume D <gdeloince.consulting@gmail.com>
2022-12-16 01:09:16 +0100

    feat(cache): add cache for article getter

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,22 +16,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
+    private ManagerRegistry $registry;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
     /**
      * @Route("/api/v1/articles", name="api_v1_article_cget", methods={"GET"})
      */
     public function cget(): JsonResponse
     {
-        $em = $this->getDoctrine()->getManager();
-
-        return $this->json($em->getRepository('App:Article')->findAll());
+        return $this->json($this->registry->getRepository(Article::class)->findAll());
     }
 
     /**
      * @Route("/api/v1/articles/{id}", name="api_v1_article_get", methods={"GET"})
      * @ParamConverter("article", class="App:Article")
      */
-    public function getOne(Article $article): JsonResponse
+    public function getOne(Article $article, Request $request): JsonResponse
     {
+        $response = new JsonResponse();
+
+        $authorUpdatedAt = $article->getAuthor()->getUpdatedAt();
+        $articleUpdatedAt = $article->getUpdatedAt();
+
+        $response
+            ->setLastModified($articleUpdatedAt > $authorUpdatedAt ? $articleUpdatedAt : $authorUpdatedAt)
+            ->setPublic()
+        ;
+
+        if ($response->isNotModified($request)) {
+            return $response->setStatusCode(Response::HTTP_NOT_MODIFIED);
+        }
+
         return $this->json($article);
     }
 
